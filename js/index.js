93b592f0322b68b852853e75ae40c162370931b9
Karetnikova Valeriya <valeriya.karetnikova@gmail.com>
2021-04-28 15:45:43 -0600

    finished

--- js/index.js
+++ js/index.js
@@ -2,37 +2,50 @@ window.addEventListener('load', function(e){
 
     const secondHand = document.querySelectorAll('.second-hand');
     const minsHand = document.querySelectorAll('.min-hand');
-  
-    console.log(secondHand);
-    console.log(minsHand);
     
     function setDate() {
       const now = new Date();
+      
       const seconds = now.getSeconds();
       const secondsDegrees = ((seconds / 60) * 360) + 90;
       const mins = now.getMinutes();
       const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-      const hour = now.getHours();
-      const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
 
       secondHand.forEach(hand => hand.style.transform = `rotate(${secondsDegrees}deg)`);
-      
       minsHand.forEach(hand => hand.style.transform = `rotate(${minsDegrees}deg)`);
 
-      //hours
-      document.querySelector('.hand.edmonton').style.transform = `rotate(${hourDegrees}deg)`;
-      document.querySelector('.hand.newyork').style.transform = `rotate(${hourDegrees+60}deg)`;
-      document.querySelector('.hand.london').style.transform = `rotate(${hourDegrees+210}deg)`;
-      document.querySelector('.hand.tokyo').style.transform = `rotate(${hourDegrees+120}deg)`;
-      document.querySelector('.hand.beijin').style.transform = `rotate(${hourDegrees+90}deg)`;
-      document.querySelector('.hand.moscow').style.transform = `rotate(${hourDegrees+300}deg)`;
-      document.querySelector('.hand.sydney').style.transform = `rotate(${hourDegrees+180}deg)`;
-      document.querySelector('.hand.brazilia').style.transform = `rotate(${hourDegrees+120}deg)`;
-      
+      //Get hours 
+      //local time in msec
+      const  localTime = now.getTime();
+      //local UTC offset (in min) convert to msec
+      const localOffset = now.getTimezoneOffset()*60*1000;
+      //utc time in msec
+      const utcTime = localTime + localOffset;
+
+      //TimeZones     
+      const timeZonesList =
+      [{name: "edmonton", offset: -6},
+       {name: "newyork", offset: -4},
+       {name: "london", offset: 1},
+       {name: "tokyo", offset: 9},
+       {name: "beijing", offset: 8},
+       {name: "moscow", offset: 3},
+       {name: "sydney", offset: 10},
+       {name: "brasilia", offset: -3}
+      ]
+
+      timeZonesList.forEach(timeZone => {
+          const destinationOffset  = timeZone.offset;
+          const destinationTime = utcTime + (3600000*destinationOffset);
+          const destinationDate = new Date(destinationTime);
+          const hour = destinationDate.getHours();
+          const min = destinationDate.getMinutes();
+          const hourDegrees = ((hour / 12) * 360) + ((min/60)*30) + 90;
+
+          document.querySelector('.hand.'+ timeZone.name).style.transform = `rotate(${hourDegrees}deg)`;
+      })
     }
     
     setInterval(setDate, 1000);
-    
     setDate();
-
 })
